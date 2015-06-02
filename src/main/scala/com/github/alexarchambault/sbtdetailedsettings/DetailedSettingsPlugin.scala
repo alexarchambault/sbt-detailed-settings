aa1170ff68c53b5706a4f968479228421971cb88
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-06-03 01:37:51 +0200

    Rework

--- src/main/scala/com/github/alexarchambault/sbtdetailedsettings/DetailedSettingsPlugin.scala
+++ src/main/scala/com/github/alexarchambault/sbtdetailedsettings/DetailedSettingsPlugin.scala
@@ -4,33 +4,58 @@ import sbt._, Keys._
 
 object DetailedSettingsPlugin extends AutoPlugin {
 
-  case class ProjectID(organization: String, name: String, version: String)
+  case class Module(organization: String,
+                    name: String,
+                    version: String,
+                    classifiers: String)
+
+  case class Projects(ids: List[String],
+                      default: String)
+
+  case class ProjectModule(projectId: String,
+                           projectDependencies: List[String],
+                           module: Module)
+
+  case class ProjectModuleSettings(projectId: String,
+                                   module: Module,
+                                   dependencies: List[Module],
+                                   exportedProducts: List[String],
+                                   unmanagedClasspath: List[String])
 
   object autoImport {
-    val detailedProjectID = taskKey[ProjectID]("Detailed project ID")
-    val detailedModuleSettings = taskKey[ModuleSettings]("Detailed module settings")
+    val detailedProjects = taskKey[Projects]("Detailed projects")
+    val detailedProjectModule = taskKey[ProjectModule]("Detailed project module")
+    val detailedProjectModuleSettings = taskKey[ProjectModuleSettings]("Detailed project module settings")
   }
 
   import autoImport._
 
   override def trigger = allRequirements
 
-  def mapModuleId(id: ModuleID, scalaBinaryVersion: String, scalaVersion: String): ModuleID =
-    id.crossVersion match {
+  def mapModuleId(id: ModuleID, scalaBinaryVersion: String, scalaVersion: String): Module = {
+    val m = id.crossVersion match {
       case _: CrossVersion.Disabled.type => id
       case b: CrossVersion.Binary => id.copy(name = id.name + "_" + b.remapVersion(scalaBinaryVersion), crossVersion = CrossVersion.Disabled)
       case f: CrossVersion.Full => id.copy(name = id.name + "_" + f.remapVersion(scalaVersion), crossVersion = CrossVersion.Disabled)
     }
 
-  def mapExclusionRule(id: SbtExclusionRule, scalaBinaryVersion: String, scalaVersion: String): SbtExclusionRule =
-    id.crossVersion match {
-      case _: CrossVersion.Disabled.type => id
-      case b: CrossVersion.Binary => id.copy(name = id.name + "_" + b.remapVersion(scalaBinaryVersion), crossVersion = CrossVersion.Disabled)
-      case f: CrossVersion.Full => id.copy(name = id.name + "_" + f.remapVersion(scalaVersion), crossVersion = CrossVersion.Disabled)
-    }
+    Module(m.organization, m.name, m.revision, m.configurations.getOrElse(""))
+  }
 
-  override lazy val projectSettings = Seq(
-    detailedProjectID := {
+  val detailedProjectsInit = Seq[Setting[_]](
+    detailedProjects := {
+      val bs = buildStructure.value
+      Projects(
+        bs.allProjectRefs.map(_.project).toList,
+        bs.units(bs.root).root
+      )
+    }
+  )
+
+  override lazy val globalSettings = detailedProjectsInit
+
+  override lazy val projectSettings = detailedProjectsInit ++ Seq(
+    detailedProjectModule := {
       val (scalaVersionValue, scalaBinaryVersionValue) =
         moduleSettings.value match {
           case settings: InlineConfigurationWithExcludes =>
@@ -42,33 +67,33 @@ object DetailedSettingsPlugin extends AutoPlugin {
         }
 
       val m = mapModuleId(projectID.value, scalaBinaryVersionValue, scalaVersionValue)
-      ProjectID(m.organization, m.name, m.revision)
+      ProjectModule(
+        thisProjectRef.value.project,
+        thisProject.value.dependencies.map(_.project).collect{case ProjectRef(_, id) => id}.toList,
+        m
+      )
     },
-    detailedModuleSettings := {
-      moduleSettings.value match {
-        case settings: InlineConfigurationWithExcludes =>
-          val scalaVersionValue = settings.ivyScala.map(_.scalaFullVersion) getOrElse scalaVersion.value
-          val scalaBinaryVersionValue = settings.ivyScala.map(_.scalaBinaryVersion) getOrElse scalaBinaryVersion.value
+    detailedProjectModuleSettings <<= Def.task {
+      val (module, deps) =
+        moduleSettings.value match {
+          case settings: InlineConfigurationWithExcludes =>
+            val scalaVersionValue = settings.ivyScala.map(_.scalaFullVersion) getOrElse scalaVersion.value
+            val scalaBinaryVersionValue = settings.ivyScala.map(_.scalaBinaryVersion) getOrElse scalaBinaryVersion.value
 
-          def map(id: ModuleID) = mapModuleId(id, scalaBinaryVersionValue, scalaVersionValue)
-          def mapExclRule(id: SbtExclusionRule) = mapExclusionRule(id, scalaBinaryVersionValue, scalaVersionValue)
+            def map(id: ModuleID) = mapModuleId(id, scalaBinaryVersionValue, scalaVersionValue)
 
-          InlineConfigurationWithExcludes(
-            map(settings.module),
-            settings.moduleInfo,
-            settings.dependencies.map(map),
-            settings.overrides.map(map),
-            settings.excludes.map(mapExclRule),
-            settings.ivyXML,
-            settings.configurations,
-            settings.defaultConfiguration,
-            settings.ivyScala,
-            settings.validate,
-            settings.conflictManager
-          )
+            (map(settings.module), settings.dependencies.map(map).toList)
 
-        case other => other
-      }
+          case other =>
+            throw new Exception(s"Unsupported module settings: $other")
+        }
+
+      ProjectModuleSettings(
+        thisProjectRef.value.project,
+        module, deps,
+        (exportedProducts in Compile).value.toList.map(_.data.toString),
+        (unmanagedClasspath in Compile).value.toList.map(_.data.toString)
+      )
     }
   )
 
